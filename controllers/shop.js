10b5fe8ef977ed5d2726ed5024df25bd4a2d68c1
tusharkarlawar <tusharkarlawar95@gmail.com>
2023-10-04 17:35:21 +0530

    modifiedUsingMongoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -44,6 +44,7 @@ exports.getIndex = (req, res, next) => {
     });
 };
 
+
 exports.getCart = (req, res, next) => {
   req.user
     .populate('cart.items.productId')
@@ -76,7 +77,7 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .deleteItemFromCart(prodId)
+    .removeFromCart(prodId)
     .then(result => {
       res.redirect('/cart');
     })
